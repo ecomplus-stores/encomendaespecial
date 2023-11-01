1da3561f8cbb4e31188ba46ecc057d268af5f1fe
matheusgnreis <matheus@e-com.club>
2023-10-31 22:13:17 -0300

    chore: update layout

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,6 @@
 // Add your custom JavaScript for storefront pages here.
+/* if (window.innerWidth > 768) {
+    $('#mobile-search-btn').on('click', () => {
+        $('#search-input').trigger('focus')
+    })
+} */
